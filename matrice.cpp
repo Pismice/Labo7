53dea6847ab3640d72e76ea705eb4a47e25b7a5e
pismice <pismice15@gmail.com>
2021-12-08 18:33:27 +0100

    galere pour sommeColonne

--- matrice.cpp
+++ matrice.cpp
@@ -4,14 +4,37 @@
 
 #include "matrice.h"
 #include "vecteur.h"
+#include <algorithm>
 
 using namespace std;
 
+void plusGrandeValeur(Vecteur v)
+{
+   v.size();
+}
+
 Vecteur sommeLigne(const Matrice& matrice)
 {
-   Vecteur vecteur(matrice.size());
-   transform(matrice.begin(), matrice.end(), vecteur.begin(), somme);
+   // La taille du vecteur correspond au nombre de vecteurs (lignes) dans la matrice
+   Vecteur reponse(matrice.size());
 
-   return vecteur;
+   transform(matrice.begin(), matrice.end(), reponse.begin(), somme);
+
+   return reponse;
+}
+
+Vecteur sommeColonne(const Matrice& matrice)
+{
+   // Trouver le plus grand vecteur de la matrice
+   // for_each(matrice.begin(), matrice.end(), plusGrandeValeur);
+   Vecteur longeurDesLignes(matrice.size());
+
+
+   // TODO: LA TAILLE DE REPONSE DOIT CORRESPONDRE AU PLUS LONG VECTEUR
+   Vecteur reponse(matrice.size());
+
+
+
+   return reponse;
 }
 
